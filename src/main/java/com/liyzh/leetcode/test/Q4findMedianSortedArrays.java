0835fb7956d674f528a462280600a34b7e6004cb
liyzh <lyz1989927@163.com>
2020-07-03 18:57:22 +0800

    init

---
+++ src/main/java/com/liyzh/leetcode/test/Q4findMedianSortedArrays.java
@@ -0,0 +1,36 @@
+package com.liyzh.leetcode.test;
+
+public class Q4findMedianSortedArrays {
+
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        int l = (m + n + 1) / 2;
+        int r = (m + n + 2) / 2;
+        return (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0;
+    }
+
+    public double getkth(int[] A, int aStart, int[] B, int bStart, int k) {
+        if (aStart > A.length - 1) return B[bStart + k - 1];
+        if (bStart > B.length - 1) return A[aStart + k - 1];
+        if (k == 1) return Math.min(A[aStart], B[bStart]);
+
+        int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
+        if (aStart + k / 2 - 1 < A.length) aMid = A[aStart + k / 2 - 1];
+        if (bStart + k / 2 - 1 < B.length) bMid = B[bStart + k / 2 - 1];
+
+        if (aMid < bMid)
+            return getkth(A, aStart + k / 2, B, bStart, k - k / 2);// Check: aRight + bLeft
+        else
+            return getkth(A, aStart, B, bStart + k / 2, k - k / 2);// Check: bRight + aLeft
+    }
+
+    public static void main(String[] args) {
+        Q4findMedianSortedArrays test = new Q4findMedianSortedArrays();
+//        int[] A = new int[]{1, 3};
+//        int[] B = new int[]{2};
+        int[] A = new int[]{1, 2};
+        int[] B = new int[]{3, 4};
+        double median = test.findMedianSortedArrays(A, B);
+        System.out.println(median);
+    }
+}
