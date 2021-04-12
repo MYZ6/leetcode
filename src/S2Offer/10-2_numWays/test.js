29c265fba3f28e4584993f4ec4d67256d7fa4b77
liyzh <lyz1989927@163.com>
2021-04-12 10:28:01 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

--- src/S2Offer/10-2_numWays/test.js
+++ src/S2Offer/10-2_numWays/test.js
@@ -1,60 +1,46 @@
 /**
- * 剑指 Offer 10- I. 斐波那契数列
- 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
-
- F(0) = 0,   F(1) = 1
- F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
- 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * 剑指 Offer 10- II. 青蛙跳台阶问题
+ 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
 
  答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
-
-
 
  示例 1：
 
  输入：n = 2
- 输出：1
+ 输出：2
  示例 2：
 
- 输入：n = 5
- 输出：5
+ 输入：n = 7
+ 输出：21
+ 示例 3：
 
-
+ 输入：n = 0
+ 输出：1
  提示：
 
  0 <= n <= 100
+ 注意：本题与主站 70 题相同：https://leetcode-cn.com/problems/climbing-stairs/
  */
-
 /**
  * @param {number} n
  * @return {number}
  */
-var fib_arr = function (n) {
-    const farr = [0n, 1n]; // bigInt
+var numWays = function (n) {
+    const farr = [1n, 1n]; // bigInt
     for (let i = 2; i <= n; i++) {
         farr[i] = farr[i - 1] + farr[i - 2];
     }
     console.log(farr)
     return farr[n] % 1000000007n;
 };
-var fib = function (n) {
-    let a = 0, b = 1, sum;
-    for (let i = 0; i < n; i++) {
-        sum = a + b;
-        a = b;
-        b = sum;
-        console.log(sum)
-    }
-    return a % 1000000007;
-};
-var fib2 = function (n) {
+var numWays2 = function (n) {
     if (n === 0) {
-        return 0;
+        return 1; // 题目示例，说明 f(0) = 1
     }
     if (n === 1) {
         return 1;
     }
-    let a = 0, b = 1, sum;
+    let a = 1, b = 1, sum;
     for (let i = 2; i <= n; i++) {
         // sum = a + b;
         sum = (a + b) % 1000000007;
@@ -69,10 +55,10 @@ var fib2 = function (n) {
 
 console.log(Number.MAX_SAFE_INTEGER);
 
-const n = 99;
-let result = fib_arr(n)
+const n = 0;
+let result = numWays(n)
 console.log(result);
 // result = fib(n);
 // console.log(result);
-result = fib2(n);
+result = numWays2(n);
 console.log(result);
